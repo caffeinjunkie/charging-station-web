64b56a91fb1d8ee5ad9bbe09bb722e2b80a15931
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-06-12 02:42:33 +0700

    feature/location-list: refactor util impor

--- src/util/locales/i18n.ts
+++ src/util/locales/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 
-import en from './locales/en.json'
-import nl from './locales/nl.json'
+import en from './en.json'
+import nl from './nl.json'
 
 i18n.init({
   resources: {
