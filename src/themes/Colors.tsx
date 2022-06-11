f442ae66b64fdafff20ec04b1583b076488504b1
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-06-11 22:45:36 +0700

    feature/location-list: add assets and themes

---
+++ src/themes/Colors.tsx
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: '#ffda39',
+  white: '#ededed',
+  black: '#121212',
+  error: '#ec0412',
+  success: '#04bc2b',
+  softRed: '#fa6063',
+  lightGrey: '#BEBEBE',
+  grey: '#808080',
+  darkGrey: '#585858',
+  shadowColor: '#25170e44'
+}
