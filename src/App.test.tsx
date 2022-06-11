f56be0abc884e2f79f8a1ce7d1623d6ec9fbe490
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-06-11 22:50:05 +0700

    feature/location-list: add header to app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,23 +3,20 @@ import { shallow } from 'enzyme';
 import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
+import { Header } from './components/Header';
 
 describe('App', () => {
-  let wrapper;
-  let rootNavigation: any;
-  
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-    
-    rootNavigation = wrapper.find('RootNavigation');
-  });
-  
   afterEach(() => {
     jest.clearAllMocks();
   });
   
   describe('#render', () => {
-    it('should render root navigation and browser router', () => {
+    it('should render header, root navigation and browser router', () => {
+      const wrapper = shallow(<App />);
+      const rootNavigation = wrapper.find('RootNavigation');
+      const headerComponent = wrapper.find(Header);
+      
+      expect(headerComponent).toHaveLength(1);
       expect(rootNavigation).toHaveLength(1);
       expect(BrowserRouter).toHaveLength(1);
     });
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import RootNavigation from './root/RootNavigation';
+import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className="App">
         <RootNavigation />
       </div>
