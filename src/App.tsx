cdbce05799525f57cf4bdc3cd91e2e1bdcc16bb7
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-06-11 22:51:25 +0700

    feature/location-list: remove app css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import RootNavigation from './root/RootNavigation';
-import './App.css';
 import { Header } from './components/Header';
 
 function App() {
